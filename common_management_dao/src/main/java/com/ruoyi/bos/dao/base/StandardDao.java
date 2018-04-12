f3ecf490d691a8df7380b028b2ad7d97dc93a1d4
ruoyi5268 <1004662722@qq.com>
2018-04-12 23:52:50 +0800

    spring data jpa

---
+++ common_management_dao/src/main/java/com/ruoyi/bos/dao/base/StandardDao.java
@@ -0,0 +1,16 @@
+package com.ruoyi.bos.dao.base;
+
+import com.ruoyi.bos.domain.base.Standard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author zhaohuan
+ * @version V1.0
+ * @Title: StandardDao
+ * @Description: <p>  </p>
+ * @date 2018/4/12 23:06
+ */
+
+public interface StandardDao extends JpaRepository<Standard,Integer>{
+
+}
